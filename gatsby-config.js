a911f4cdb9aa8451f2c1e150c6433b54b04eb8e9
Alejandro Castro <guardiadelcastro@gmail.com>
2019-02-25 16:02:17 +0000

    changed page title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `AleCG`,
+    description: `My personal page, created by me with GatsbyJs`,
+    author: `Alejandro Castro de la Guardia`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
